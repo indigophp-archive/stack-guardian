389596e3f6df5206967a1d2ecf4f15fbc3e6c6cd
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2015-03-19 13:58:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# Guardian Stack Middleware
+
+[![Latest Version](https://img.shields.io/github/release/indigophp/stack-guardian.svg?style=flat-square)](https://github.com/indigophp/stack-guardian/releases)
+[![Software License](https://img.shields.io/badge/license-MIT-brightgreen.svg?style=flat-square)](LICENSE)
+[![Build Status](https://img.shields.io/travis/indigophp/stack-guardian.svg?style=flat-square)](https://travis-ci.org/indigophp/stack-guardian)
+[![Code Coverage](https://img.shields.io/scrutinizer/coverage/g/indigophp/stack-guardian.svg?style=flat-square)](https://scrutinizer-ci.com/g/indigophp/stack-guardian)
+[![Quality Score](https://img.shields.io/scrutinizer/g/indigophp/stack-guardian.svg?style=flat-square)](https://scrutinizer-ci.com/g/indigophp/stack-guardian)
+[![HHVM Status](https://img.shields.io/hhvm/indigophp/stack-guardian.svg?style=flat-square)](http://hhvm.h4cc.de/package/indigophp/stack-guardian)
+[![Total Downloads](https://img.shields.io/packagist/dt/indigophp/stack-guardian.svg?style=flat-square)](https://packagist.org/packages/indigophp/stack-guardian)
+[![Dependency Status](https://img.shields.io/versioneye/d/php/indigophp:skeleton.svg?style=flat-square)](https://www.versioneye.com/php/indigophp:skeleton)
+
+**Guardian middleware for StackPHP.**
+
+
+## Install
+
+Via Composer
+
+``` bash
+$ composer require indigophp/stack-guardian
+```
+
+
+## Usage
+
+
+## Testing
+
+``` bash
+$ phpunit
+```
+
+
+## Contributing
+
+Please see [CONTRIBUTING](CONTRIBUTING.md) for details.
+
+
+## Credits
+
+- [Tamás Barta](https://github.com/TamasBarta)
+- [Márk Sági-Kazár](https://github.com/sagikazarmark)
+- [All Contributors](https://github.com/indigophp/stack-guardian/contributors)
+
+
+## License
+
+The MIT License (MIT). Please see [License File](LICENSE) for more information.
---
+++ src/Authentication.php
@@ -0,0 +1,93 @@
+<?php
+
+/*
+ * This file is part of the Indigo Guardian package.
+ *
+ * (c) Indigo Development Team
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Indigo\Guardian\Stack;
+
+use Indigo\Guardian\Service\Logout;
+use Indigo\Guardian\Service\Resume;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+
+/**
+ * Protect pages with authentication
+ *
+ * @author Márk Sági-Kazár <mark.sagikazar@gmail.com>
+ */
+class Authentication implements HttpKernelInterface
+{
+    /**
+     * @var HttpKernelInterface
+     */
+    protected $app;
+
+    /**
+     * @var Resume
+     */
+    protected $resumeService;
+
+    /**
+     * @var Logout
+     */
+    protected $logoutService;
+
+    /**
+     * @param HttpKernelInterface $app
+     * @param Resume              $resumeService
+     * @param Logout              $logoutService
+     */
+    public function __construct(
+        HttpKernelInterface $app,
+        Resume $resumeService,
+        Logout $logoutService
+    ) {
+        $this->app = $app;
+        $this->resumeService = $resumeService;
+        $this->logoutService = $logoutService;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function handle(Request $request, $type = HttpKernelInterface::MASTER_REQUEST, $catch = true)
+    {
+        $route = $request->getPathInfo();
+
+        // We have an active login
+        if ($this->resumeService->check()) {
+
+            switch ($route) {
+                case '/login':
+                    $returnUri = $request->query->get('uri', $request->attributes->get('stack.url_map.prefix', '/'));
+
+                    return new RedirectResponse($returnUri);
+                    break;
+                case '/logout':
+                    $this->logoutService->logout();
+
+                    return new RedirectResponse($request->attributes->get('stack.url_map.prefix', '/'));
+                    break;
+                default:
+                    $caller = $this->resumeService->getCurrentCaller();
+
+                    $request->attributes->set('stack.authn.token', $caller->getLoginToken());
+                    break;
+            }
+        } elseif($route !== '/login') {
+            $routePrefix = $request->attributes->get('stack.url_map.prefix', '');
+            $fullRoute = $request->server->get('PATH_INFO');
+
+            return new RedirectResponse(sprintf('%s/login?uri=%s', $routePrefix, $fullRoute));
+        }
+
+        return $this->app->handle($request, $type, $catch);
+    }
+}
